87e583d7f7f571a1dbe8549c3832c7ecee89a522
Jose Quiroga <jose.rojas@ing.austral.edu.ar>
2022-06-19 13:30:25 -0300

    added get element

--- appium/index.js
+++ appium/index.js
@@ -1,12 +1,13 @@
 const wdio = require("webdriverio");
+const assert = require("assert");
 const opts = {
     path: '/wd/hub',
     port: 4723,
     capabilities: {
         platformName: "Android",
         platformVersion: "8",
-        deviceName: "Pixel API 26",
-        app: "appium/apk/despegar.apk",
+        deviceName: "emulator-5554",
+        app: "/Users/JoseRojas/Projects/aseguramiento/tp-despegar/appium/apk/despegar.apk",
         appPackage: "com.gm.despegar",
         appActivity: "com.despegar.ui.splash.SplashActivity",
         automationName: "UiAutomator2"
@@ -14,23 +15,15 @@ const opts = {
 };
 
 async function main () {
-    const client = await wdio.remote(opts);
-    console.log("client: ", client);
-    //const field = await $$('com.gm.despegar:id/countryName')
-    client.refresh;
-    console.log(client.findElement);
-    const field = await client.$$("android.widget.FrameLayout")
-    console.log("field: ",field);
-    const field2 = await client.$$("android.widget.LinearLayout");
-    console.log("field2: ",field2);
-
-    const field23ViewGroup = await client.$$("id:com.gm.despegar:id/countryName");
-    console.log("Names: ",field23ViewGroup);
-
-
-    assert.strictEqual(field3,"Argentina");
-    await client.deleteSession();
-
+    const browser = await wdio.remote(opts);
+    let container;
+    await browser.waitUntil(async () => {
+        container = await browser.$('id:com.gm.despegar:id/recyclerView');
+        return container?.error === undefined;
+    }, {timeout: 5000, timeoutMsg: 'timeout'});
+    let field = await container.$$('android.widget.LinearLayout')[0].$('android.widget.TextView');
+    console.log("field: ", await field.getText());
+    await browser.deleteSession();
 }
 
 main();
